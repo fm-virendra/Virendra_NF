e587376a8715a2996fdb26a8421952185225fe56
Virendra Yadav <159771737+fm-virendra@users.noreply.github.com>
2025-04-11 01:10:49 +0530

    helloworld.java modified

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
-public class HelloWorld{
-public static void main(String[] args){
-system.out.println("HelloWorld"):
+public class helloworld {
+public static void main(String[] args) {
+    System.out.println("HelloWorld");
 }
 }
